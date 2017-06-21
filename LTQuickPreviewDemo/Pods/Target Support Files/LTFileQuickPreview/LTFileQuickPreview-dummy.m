fe88eea5ba8405862406da3d997234d62a2eca3a
liangtong <l900416@163.com>
2017-06-21 22:28:47 +0800

    update demo

---
+++ LTQuickPreviewDemo/Pods/Target Support Files/LTFileQuickPreview/LTFileQuickPreview-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LTFileQuickPreview : NSObject
+@end
+@implementation PodsDummy_LTFileQuickPreview
+@end
---
+++ LTQuickPreviewDemo/Pods/Target Support Files/LTFileQuickPreview/LTFileQuickPreview-umbrella.h
@@ -0,0 +1,10 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "LTQuckPreviewViewModel.h"
+#import "LTQuickPreviewViewController.h"
+
+FOUNDATION_EXPORT double LTFileQuickPreviewVersionNumber;
+FOUNDATION_EXPORT const unsigned char LTFileQuickPreviewVersionString[];
+
